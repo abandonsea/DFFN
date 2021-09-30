4f2ac3de7d877db5537d51f7e7649b69a77e8318
Pedro Vieira <pedrovieira@arrowplan.com>
2021-09-30 18:05:44 +0200

    Change structure of file and organization of functions to better fit the implementation of the DFFN

--- tools.py
+++ tools.py
@@ -8,7 +8,7 @@ Created on Wed Sep 29 14:29 2021
 """
 
 import torch
-import torch.utils.data as Torchdata
+from sklearn.decomposition import PCA
 import numpy as np
 from scipy import io
 import random
@@ -16,146 +16,121 @@ import os
 from tqdm import tqdm
 
 
-# Load data set
-def load_dataset(dataset_name, target_folder='./Datasets/'):
-    """Returns dataset raw image and labels"""
+# Dataset class
+class HSIDataset:
+    def __init__(self, dataset_name, target_folder='./Datasets/'):
+        """Returns dataset raw image and labels"""
 
-    folder = target_folder + dataset_name + '/'
-    if dataset_name == 'IndianPines':
-        # load the image
-        img = io.loadmat(folder + 'Indian_pines_corrected.mat')
-        img = img['indian_pines_corrected']
-        gt = io.loadmat(folder + 'Indian_pines_gt.mat')['indian_pines_gt']
-        label_values = ["Undefined", "Alfalfa", "Corn-notill", "Corn-mintill",
-                        "Corn", "Grass-pasture", "Grass-trees",
-                        "Grass-pasture-mowed", "Hay-windrowed", "Oats",
-                        "Soybean-notill", "Soybean-mintill", "Soybean-clean",
-                        "Wheat", "Woods", "Buildings-Grass-Trees-Drives",
-                        "Stone-Steel-Towers"]
-        rgb_bands = (43, 21, 11)  # AVIRIS sensor
+        folder = target_folder + dataset_name + '/'
+        self.rgb_bands = None
+        self.label_values = None
         ignored_labels = [0]
-    elif dataset_name == 'PaviaU':
-        # load the image
-        img = io.loadmat(folder + 'PaviaU.mat')['paviaU']
-        gt = io.loadmat(folder + 'PaviaU_gt.mat')['paviaU_gt']
-        label_values = ['Undefined', 'Asphalt', 'Meadows', 'Gravel', 'Trees',
-                        'Painted metal sheets', 'Bare Soil', 'Bitumen',
-                        'Self-Blocking Bricks', 'Shadows']
-        rgb_bands = (55, 41, 12)
-        ignored_labels = [0]
-    elif dataset_name == 'PaviaC':
-        # Load the image
-        img = io.loadmat(folder + 'Pavia.mat')['pavia']
+        if dataset_name == 'IndianPines':
+            img = io.loadmat(folder + 'Indian_pines_corrected.mat')['indian_pines_corrected']
+            gt = io.loadmat(folder + 'Indian_pines_gt.mat')['indian_pines_gt']
+            self.label_values = ["Undefined", "Alfalfa", "Corn-notill", "Corn-mintill",
+                                 "Corn", "Grass-pasture", "Grass-trees",
+                                 "Grass-pasture-mowed", "Hay-windrowed", "Oats",
+                                 "Soybean-notill", "Soybean-mintill", "Soybean-clean",
+                                 "Wheat", "Woods", "Buildings-Grass-Trees-Drives",
+                                 "Stone-Steel-Towers"]
+            self.rgb_bands = (43, 21, 11)  # AVIRIS sensor
+        elif dataset_name == 'PaviaU':
+            img = io.loadmat(folder + 'PaviaU.mat')['paviaU']
+            gt = io.loadmat(folder + 'PaviaU_gt.mat')['paviaU_gt']
+            self.label_values = ['Undefined', 'Asphalt', 'Meadows', 'Gravel', 'Trees',
+                                 'Painted metal sheets', 'Bare Soil', 'Bitumen',
+                                 'Self-Blocking Bricks', 'Shadows']
+            self.rgb_bands = (55, 41, 12)
+        elif dataset_name == 'Salinas':
+            img = io.loadmat(folder + 'Salinas_corrected.mat')['salinas_corrected']
+            gt = io.loadmat(folder + 'Salinas_gt.mat')['salinas_gt']
+            self.label_values = ['Undefined', 'Brocoli_green_weeds_1', 'Brocoli_green_weeds_2', 'Fallow',
+                                 'Fallow_rough_plow', 'Fallow_smooth', 'Stubble', 'Celery', 'Grapes_untrained',
+                                 'Soil_vinyard_develop', 'Corn_senesced_green_weeds', 'Lettuce_romaine_4wk',
+                                 'Lettuce_romaine_5wk', 'Lettuce_romaine_6wk', 'Lettuce_romaine_7wk',
+                                 'Vinyard_untrained', 'Vinyard_vertical_trellis']
+            self.rgb_bands = (43, 21, 11)
+        else:
+            raise ValueError("{} dataset is unknown.".format(dataset_name))
 
-        rgb_bands = (55, 41, 12)
+        # Filter NaN values
+        nan_mask = np.isnan(img.sum(axis=-1))
+        if np.count_nonzero(nan_mask) > 0:
+            print("Warning: NaN have been found in the data. "
+                  "It is preferable to remove them beforehand. "
+                  "Learning on NaN data is disabled.")
+        img[nan_mask] = 0
+        gt[nan_mask] = 0
+        self.ground_truth = gt
+        ignored_labels.append(0)
+        self.ignored_labels = list(set(ignored_labels))
 
-        gt = io.loadmat(folder + 'Pavia_gt.mat')['pavia_gt']
+        self.raw_image = np.asarray(img, dtype='float32')
 
-        label_values = ["Undefined", "Water", "Trees", "Asphalt",
-                        "Self-Blocking Bricks", "Bitumen", "Tiles", "Shadows",
-                        "Meadows", "Bare Soil"]
+    def apply_image_preprocessing(self, num_bands):
+        # TODO: implement PCA (scikit-learn)
+        num_bands = self.raw_image.shape[-1]
 
-        ignored_labels = [0]
-    elif dataset_name == 'Salinas':
-        # Load the image
-        img = io.loadmat(folder + 'Salinas_corrected.mat')['salinas_corrected']
-        gt = io.loadmat(folder + 'Salinas_gt.mat')['salinas_gt']
-        label_values = ['Undefined', 'Brocoli_green_weeds_1', 'Brocoli_green_weeds_2', 'Fallow',
-                        'Fallow_rough_plow', 'Fallow_smooth', 'Stubble', 'Celery', 'Grapes_untrained',
-                        'Soil_vinyard_develop', 'Corn_senesced_green_weeds', 'Lettuce_romaine_4wk',
-                        'Lettuce_romaine_5wk', 'Lettuce_romaine_6wk', 'Lettuce_romaine_7wk',
-                        'Vinyard_untrained', 'Vinyard_vertical_trellis']
-        rgb_bands = (43, 21, 11)
-        ignored_labels = [0]
-    elif dataset_name == 'SalinaA':
-        # Load the image
-        img = io.loadmat(folder + 'SalinasA_corrected.mat')['salinasA_corrected']
-        gt = io.loadmat(folder + 'SalinasA_gt.mat')['salinasA_gt']
-        label_values = ['Undefined', 'Brocoli_green_weeds_1', 'Corn_senesced_green_weeds',
-                        'Lettuce_romaine_4wk', 'Lettuce_romaine_5wk',
-                        'Lettuce_romaine_6wk', 'Lettuce_romaine_7wk']
-        rgb_bands = (43, 21, 11)
-        ignored_labels = [0]
-    elif dataset_name == 'KSC':
-        # Load the image
-        img = io.loadmat(folder + 'KSC.mat')['KSC']
+        # Normalize data
+        img = self.raw_image
+        for band in range(num_bands):
+            min_val = np.min(img[:, :, band])
+            max_val = np.max(img[:, :, band])
+            img[:, :, band] = (img[:, :, band] - min_val) / (max_val - min_val)
+        return img
 
-        rgb_bands = (43, 21, 11)  # AVIRIS sensor
+    def num_classes(self):
+        len(self.label_values) - len(self.ignored_labels)
+        return
 
-        gt = io.loadmat(folder + 'KSC_gt.mat')['KSC_gt']
-        label_values = ["Undefined", "Scrub", "Willow swamp",
-                        "Cabbage palm hammock", "Cabbage palm/oak hammock",
-                        "Slash pine", "Oak/broadleaf hammock",
-                        "Hardwood swamp", "Graminoid marsh", "Spartina marsh",
-                        "Cattail marsh", "Salt marsh", "Mud flats", "Wate"]
+    # Get train test split
+    def split_ground_truth(self, train_size, mode='fixed_with_one'):
+        # TODO: Optimized code and check for correctness
+        indices = np.nonzero(self.ground_truth)
+        x = list(zip(*indices))  # x,y features
+        y = self.ground_truth[indices].ravel()  # classes
+        train_gt = np.zeros_like(self.ground_truth)
+        test_gt = np.zeros_like(self.ground_truth)
+        if train_size > 1:
+            train_size = int(train_size)
+            if mode.find('random') != -1:
+                train_size = float(train_size) / 100
 
-        ignored_labels = [0]
-    else:
-        raise ValueError("{} dataset is unknown.".format(dataset_name))
+        if mode == 'random_with_one':
+            train_indices = []
+            test_gt = np.copy(self.ground_truth)
+            for c in np.unique(self.ground_truth):
+                if c == 0:
+                    continue
+                indices = np.nonzero(self.ground_truth == c)
+                x = list(zip(*indices))  # x,y features
+                train_len = int(np.ceil(train_size * len(x)))
+                train_indices += random.sample(x, train_len)
+            index = tuple(zip(*train_indices))
+            train_gt[index] = self.ground_truth[index]
+            test_gt[index] = 0
 
-    # Filter NaN out
-    nan_mask = np.isnan(img.sum(axis=-1))
-    if np.count_nonzero(nan_mask) > 0:
-        print("Warning: NaN have been found in the data. "
-              "It is preferable to remove them beforehand. "
-              "Learning on NaN data is disabled.")
-    img[nan_mask] = 0
-    gt[nan_mask] = 0
-    ignored_labels.append(0)
+        elif mode == 'fixed_with_one':
+            train_indices = []
+            test_gt = np.copy(self.ground_truth)
+            for c in np.unique(self.ground_truth):
+                if c == 0:
+                    continue
+                indices = np.nonzero(self.ground_truth == c)
+                x = list(zip(*indices))  # x,y features
 
-    ignored_labels = list(set(ignored_labels))
-    # Normalization
-    img = np.asarray(img, dtype='float32')
-    n_bands = img.shape[-1]
-    for band in range(n_bands):
-        min_val = np.min(img[:, :, band])
-        max_val = np.max(img[:, :, band])
-        img[:, :, band] = (img[:, :, band] - min_val) / (max_val - min_val)
-    return img, gt, label_values, ignored_labels, rgb_bands
+                train_indices += random.sample(x, train_size)
+            index = tuple(zip(*train_indices))
+            train_gt[index] = self.ground_truth[index]
+            test_gt[index] = 0
+        else:
+            raise ValueError("{} sampling is not implemented yet.".format(mode))
+        return train_gt, test_gt
 
-
-# Get train test split
-def sample_gt(gt, train_size, mode='fixed_with_one'):
-    indices = np.nonzero(gt)
-    x = list(zip(*indices))  # x,y features
-    y = gt[indices].ravel()  # classes
-    train_gt = np.zeros_like(gt)
-    test_gt = np.zeros_like(gt)
-    if train_size > 1:
-        train_size = int(train_size)
-        if mode == 'random':
-            train_size = float(train_size) / 100  # dengbin:20181011
-
-    if mode == 'random_with_one':
-        train_indices = []
-        test_gt = np.copy(gt)
-        for c in np.unique(gt):
-            if c == 0:
-                continue
-            indices = np.nonzero(gt == c)
-            x = list(zip(*indices))  # x,y features
-            train_len = int(np.ceil(train_size * len(x)))
-            train_indices += random.sample(x, train_len)
-        index = tuple(zip(*train_indices))
-        train_gt[index] = gt[index]
-        test_gt[index] = 0
-
-    elif mode == 'fixed_with_one':
-        train_indices = []
-        test_gt = np.copy(gt)
-        for c in np.unique(gt):
-            if c == 0:
-                continue
-            indices = np.nonzero(gt == c)
-            x = list(zip(*indices))  # x,y features
-
-            train_indices += random.sample(x, train_size)
-        index = tuple(zip(*train_indices))
-        train_gt[index] = gt[index]
-        test_gt[index] = 0
-    else:
-        raise ValueError("{} sampling is not implemented yet.".format(mode))
-    return train_gt, test_gt
+    def get_train_test_samples(self):
+        # TODO: Get train and test samples for training (use matlab code as inspiration)
+        return
 
 
 # Get samples for every run
@@ -174,3 +149,53 @@ def save_sample(train_gt, test_gt, dataset_name, sample_size, run):
         os.makedirs(sample_dir)
     sample_file = sample_dir + 'sample' + str(sample_size) + '_run' + str(run) + '.mat'
     io.savemat(sample_file, {'train_gt': train_gt, 'test_gt': test_gt})
+
+# Adapts matlab code
+# def generate_index(dataset, dataset_gt, no_classes):
+#     per_class_num = []
+#     if dataset == 'indian_pines':
+#         per_class_num = [5, 143, 83, 24, 49, 73, 3, 48, 2, 98, 245, 60, 21, 126, 39, 10]
+#         # For Indian Pines: 10 % sampling.
+#     elif dataset == 'paviau':
+#         per_class_num = [132, 372, 42, 62, 27, 101, 27, 74, 19]
+#         # For University of Pavia: 2 % sampling.
+#     elif dataset == 'salinas':
+#         per_class_num = [11, 19, 10, 7, 14, 20, 18, 57, 32, 17, 6, 10, 5, 6, 37, 10]
+#         # For Salinas: 0.5 % sampling.
+#
+#     Train_Label = []
+#     Train_index = []
+#     train_data = []
+#     test_data = []
+#     train_label = []
+#     test_label = []
+#     train_index = []
+#     test_index = []
+#     index_len = []
+#
+#     for class_id in range(1, no_classes + 1):
+#         label_gt = np.array(dataset_gt)
+#         index_ii = np.where(label_gt == class_id)
+#         rand_order = range(index_ii.shape[0]) # TODO: Random permutation
+#         class_ii = np.ones(index_ii.shape[0]) * class_id
+#         Train_Label.append(class_ii)
+#         Train_index.append(index_ii)
+#
+#         num_train = per_class_num[class_id]
+#         # num_train = floor(length(index_ii) * percent)
+#         train_ii = rand_order(:, 1: num_train)
+#         train_index = [train_index index_ii(train_ii)]
+#         # train_index = [train_index index_ii(train_ii)]
+#
+#         test_index_temp = index_ii
+#         test_index_temp(:, train_ii)=[]
+#         test_index = [test_index test_index_temp]
+#         # test_index = [test_index test_index_temp]
+#
+#         train_label = [train_label class_ii(:, 1: num_train)]
+#         test_label = [test_label class_ii(num_train + 1:end)]
+#
+#     unlabeled_index = find(label_gt == 0)
+#     order = randperm(length(unlabeled_index))
+#     unlabeled_index = unlabeled_index(order)
+#     unlabeled_label = zeros(1, length(unlabeled_index))
