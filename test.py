c71b0c0b2b9f58636a5836b75e3b62df616f729f
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-06 08:24:41 +0200

    Add basic test implementation

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ Created on Wed Oct 5 17:57 2021
 """
 
 import torch
+import torch.nn.functional as f
 import torch.utils.data as Torchdata
 import numpy as np
 from tqdm import tqdm
@@ -15,15 +16,48 @@ from tqdm import tqdm
 from tools import *
 from net import *
 
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 # Parameters setting
 DATASET = 'PaviaU'  # PaviaU; Salinas; KSC
 FOLDER = './Datasets/'  # the dataset folder
 CHECKPOINT_FOLDER = 'checkpoints/' + DATASET + '/'
+BATCH_SIZE = 20  # Batch size for every test iteration
 
 
 # Test DFFN
-def test():
-    print('Dummy!')
+def test(test_loader=None, model=None, batch_size=BATCH_SIZE):
+    # Load data if none is provided
+    if test_loader is None or model is None:
+        test_loader, model = load_test_environment()
+
+    # Begin testing
+    labels_pr = []
+    prediction_pr = []
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        for images, labels in test_loader:
+            # Get input and compute model output
+            images = images.to(device)
+            labels = labels.to(device)
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs, 1)
+            n_samples += labels.shape[0]
+            n_correct += (predicted == labels).sum().item()
+
+            class_predictions = [f.softmax(output, dim=0) for output in outputs]
+
+            prediction_pr.append(class_predictions)
+            labels_pr.append(predicted)
+
+        prediction_pr = torch.cat([torch.stack(batch) for batch in prediction_pr])
+        labels_pr = torch.cat(labels_pr)
+
+        acc = 100.0 * n_correct / n_samples
+        print(f'accuracy = {acc}')
 
 
 # Main for running test independently
