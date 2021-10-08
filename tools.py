eee6651c740827fd8bff091a23a6bd45163d9ce1
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-08 18:37:01 +0200

    Remove HSIDataset and load_checkpoint from tools

--- tools.py
+++ tools.py
@@ -8,13 +8,13 @@ Created on Wed Sep 29 14:29 2021
 """
 
 import torch
-from torch.utils.data import Dataset
 import numpy as np
 from scipy import io
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import random
 import os
+import glob
 
 
 # Dataset class
@@ -158,70 +158,3 @@ class HSIData:
             os.makedirs(split_folder)
         sample_file = split_folder + train_size + val_size + 'run_' + str(run) + '.mat'
         io.savemat(sample_file, {'train_gt': train_gt, 'test_gt': test_gt, 'val_gt': val_gt})
-
-
-# Dataset class based on PyTorch's
-class HSIDataset(Dataset):
-    """Dataset class based on PyTorch's"""
-
-    def __init__(self, data, gt, sample_size=23, data_augmentation=True):
-        super(HSIDataset, self).__init__()
-        self.sample_size = sample_size
-        self.data_augmentation = data_augmentation
-
-        # Add padding according to sample_size
-        pad_size = sample_size // 2
-        self.data = np.pad(data, ((pad_size, pad_size), (pad_size, pad_size), (0, 0)), mode='symmetric')
-        self.label = np.pad(gt, pad_size, mode='constant')
-
-        # Get indices to data based on ground-truth
-        self.indices = []
-        for c in np.unique(self.label):
-            if c == 0:
-                continue
-            class_indices = np.nonzero(self.label == c)
-            index_tuples = list(zip(*class_indices))
-            self.indices += index_tuples
-
-    def __len__(self):
-        return len(self.indices)
-
-    def __getitem__(self, i):
-        x, y = self.indices[i]
-        pad_size = self.sample_size // 2
-        x1, y1, x2, y2 = x - pad_size, y - pad_size, x + pad_size + 1, y + pad_size + 1
-        data = self.data[x1:x2, y1:y2]
-        label = self.label[x, y] - 1  # Subtract one to ignore label 0 (unlabeled)
-
-        if self.data_augmentation:
-            # Perform data augmentation
-            data = self.apply_data_augmentation(data)
-
-        # Transpose data for it to match the expected torch dimensions
-        data = np.asarray(np.copy(data).transpose((2, 0, 1)), dtype='float32')
-        label = np.asarray(np.copy(label), dtype='int64')
-
-        # Load the data into PyTorch tensors
-        data = torch.from_numpy(data)
-        label = torch.from_numpy(label)
-        return data, label
-
-    @staticmethod
-    def apply_data_augmentation(data):
-        # Probability of flipping data
-        p1 = np.random.random()
-        if p1 < 0.334:
-            data = np.fliplr(data)
-        elif p1 < 0.667:
-            data = np.flipud(data)
-
-        # Probability of rotating image
-        p2 = np.random.random()
-        if p2 < 0.25:
-            data = np.rot90(data)
-        elif p2 < 0.5:
-            data = np.rot90(data, 2)
-        elif p2 < 0.75:
-            data = np.rot90(data, 3)
-
-        return data
