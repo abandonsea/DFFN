d0faec2668044873690d46c428717395efe9a0ad
Pedro Vieira <pedrovieira@arrowplan.com>
2021-10-15 11:08:20 +0200

    Add bash file to download datasets

---
+++ download_datasets.sh
@@ -0,0 +1,2 @@
+curl -o ./datasets/PaviaU/PaviaU.mat http://www.ehu.eus/ccwintco/uploads/e/ee/PaviaU.mat
+curl -o ./datasets/PaviaU/PaviaU_gt.mat http://www.ehu.eus/ccwintco/uploads/5/50/PaviaU_gt.mat
