0f62d4bde460c04fd7affe1ffcce9304cb6a1dc1
Pedro Vieira <pedrovieira@arrowplan.com>
2021-12-21 14:33:32 +0100

    Add scripts to test the noise cases

---
+++ noise_test.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 15:33 2021
+
+@author: Pedro Vieira
+@description: Implements the test function for the SAE-3DDRN network adding noise to the input image
+"""
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import numpy as np
+
+import net.dffn as dffn_pavia
+import net.dffn_salinas as dffn_salinas
+import net.dffn_indian_pines as dffn_indian
+from test import test_model
+from utils.config import DFFNConfig
+from utils.dataset import DFFNDataset
+from utils.noise import add_noise
+from utils.tools import *
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+########################
+# SET TEST CONFIG FILE #
+########################
+PATH = 'experiments/'
+EXPERIMENTS = ['server_salinas_01']
+
+NOISES = [['salt_and_pepper', 0], ['salt_and_pepper', 0.001], ['salt_and_pepper', 0.005], ['salt_and_pepper', 0.01],
+          ['salt_and_pepper', 0.05],
+          ['additive_gaussian', 0.05], ['additive_gaussian', 0.1], ['additive_gaussian', 0.3],
+          ['additive_gaussian', 0.5], ['additive_gaussian', 1.0],
+          ['multiplicative_gaussian', 0.1], ['multiplicative_gaussian', 0.3], ['multiplicative_gaussian', 0.5],
+          ['multiplicative_gaussian', 1.0]]
+
+
+# Test SAE-3DDRN runs
+def test(config_file):
+    # Load config data from training
+    cfg = DFFNConfig(config_file, test=True)
+
+    # Set string modifier if testing best models
+    test_best = 'best_' if cfg.test_best_models else ''
+    if cfg.test_best_models:
+        print('Testing best models from each run!')
+
+    # Load raw dataset, apply PCA and normalize dataset.
+    data = HSIData(cfg.dataset, cfg.data_folder, cfg.sample_bands)
+
+    for run in range(cfg.num_runs):
+        print(f'TESTING RUN {run + 1}/{cfg.num_runs}')
+
+        _, test_gt, _ = HSIData.load_samples(cfg.split_folder, cfg.train_split, cfg.val_split, run)
+        num_classes = len(np.unique(test_gt)) - 1  # Remove one for the "undefined" class
+
+        for noise in NOISES:
+            print(f'Using {noise[0]} noise with parameter: {noise[1]}')
+            noisy_data = add_noise(data.raw_image, noise)
+            pca_noisy_data = data.apply_transforms(noisy_data)
+
+            # Load test ground truth and initialize test loader
+            test_dataset = DFFNDataset(pca_noisy_data, test_gt, cfg.sample_size, data_augmentation=False)
+            test_loader = DataLoader(test_dataset, batch_size=cfg.test_batch_size, shuffle=False)
+
+            # Load model
+            model_file = f'{cfg.exec_folder}runs/vscnn_{test_best}model_run_{run}.pth'
+            if cfg.dataset == 'paviau':
+                model = nn.DataParallel(dffn_pavia.DFFN(cfg.sample_bands, num_classes))
+            elif cfg.dataset == 'salinas':
+                model = nn.DataParallel(dffn_salinas.DFFN(cfg.sample_bands, num_classes))
+            else:  # indian pines
+                model = nn.DataParallel(dffn_indian.DFFN(cfg.sample_bands, num_classes))
+
+            model.load_state_dict(torch.load(model_file, map_location=device))
+            model.eval()
+
+            # Set model to device
+            model = model.to(device)
+
+            # Test model from the current run
+            report = test_model(model, test_loader)
+            save_noise_results(cfg.results_folder, noise, report)
+
+
+# Main for running test independently
+def main():
+    # Load experiments
+    for experiment in EXPERIMENTS:
+        config_file = PATH + experiment + '/config.yaml'
+
+        test(config_file)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/noise.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 7 13:31 2021
+
+@author: Pedro Vieira
+@description: Implements a function to add noise to a test hyperspectral image
+"""
+
+import torch
+import numpy as np
+
+from utils.tools import HSIData
+
+# Set image file path
+PATH = '../experiments/prototype/'
+IMAGE_NAME = 'proc_image.pth'
+
+# Set noise parameters
+NOISE_TYPE = 'salt_and_pepper'
+NOISE_AMOUNT = 0.1
+
+
+def add_noise(img, noise_params):
+    noise_type = noise_params[0]
+    noise_amount = noise_params[1]
+    if noise_amount == 0:
+        return img
+
+    # Set out data to 0 mean and 1 variance to apply noise, then undo normalization
+    out = np.copy(img)
+    out, scaler = HSIData.normalize(out, 'standard')
+
+    max_value = np.max(out)
+    min_value = np.min(out)
+
+    # Applies a salt and pepper noise to each band for a noise_param ratio of the pixels
+    # noise_param = fraction of noisy pixels [0.1)
+    if noise_type == 'salt_and_pepper':
+        # Calculate the number of image salt and pepper noise points
+        pixels_per_band = img.shape[0] * img.shape[1]
+        num_points = int(np.floor(pixels_per_band * noise_amount))
+
+        for band in range(img.shape[2]):
+            for i in range(num_points):
+                rand_x = np.random.randint(img.shape[0])  # Generate random x coordinate
+                rand_y = np.random.randint(img.shape[1])  # Generate random y coordinate
+
+                out[rand_x, rand_y, band] = min_value if np.random.random() <= 0.5 else max_value
+
+    # Applies an additive gaussian noise to every pixel with mean and variance defined by noise_param
+    # noise_param = sigma; normal = [mu, sigma]
+    elif noise_type == 'additive_gaussian':
+        for idx in range(out.shape[2]):
+            noise = np.random.normal(0.0, noise_amount, size=(img.shape[0], img.shape[1]))
+            out[:, :, idx] += noise
+
+    # Applies a multiplicative gaussian noise to every pixel with mean and variance defined by noise_param
+    # noise_param = sigma; noise = normal(1.0, noise_param)
+    elif noise_type == 'multiplicative_gaussian':
+        for idx in range(out.shape[2]):
+            noise = np.random.normal(1.0, noise_amount, size=(img.shape[0], img.shape[1]))
+            out[:, :, idx] = np.multiply(out[:, :, idx], noise)
+    else:
+        raise Exception('Noise type not implemented')
+
+    # Prune out of bounds values
+    out_of_bounds_max = np.nonzero(out > max_value)
+    out[out_of_bounds_max] = max_value
+    out_of_bounds_min = np.nonzero(out < min_value)
+    out[out_of_bounds_min] = min_value
+
+    out = HSIData.denormalize(out, scaler)
+
+    return out
+
+
+# Main for running script independently
+def main():
+    img = torch.load(PATH + IMAGE_NAME)
+
+    noisy_img = add_noise(img, [NOISE_TYPE, NOISE_AMOUNT])
+    torch.save(noisy_img, PATH + 'img_noise_' + NOISE_TYPE + '.pth')
+
+
+if __name__ == '__main__':
+    main()
